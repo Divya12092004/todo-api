da5e4edfc12442ca98c05ba7ed5539169f59f1e7
Divya12092004 <144607306+Divya12092004@users.noreply.github.com>
2025-10-15 23:26:28 +0530

    Add files via upload

--- node server.js
+++ node server.js
@@ -40,4 +40,8 @@ app.delete("/api/todos/completed", (req, res) => {
 });
 
 // Start the server
-app.listen(5000, () => console.log("✅ Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000; // Use Render's PORT or default 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
